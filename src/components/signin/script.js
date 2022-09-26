8fe1fed4168c0b6b33747a0add7fa08bf8dc5211
Saketh0610 <110999583+Saketh0610@users.noreply.github.com>
2022-09-26 19:39:44 +0530

    Update script.js

--- src/components/signin/script.js
+++ src/components/signin/script.js
@@ -1,54 +1,32 @@
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
+function signin()
+{
+    //for Aadhar Number
+    var aadhar = document.forms["sform"]["aadhar"].value;
+    var errorsAadhar = document.getElementById('aadharError');
 
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
+    //for Username/Mobile
+    var mobile = document.forms["sform"]["mobile"].value;
+    var errorsMobile = document.getElementById('mobileError');
+
+    //for Password
+    var passwd = document.forms["sform"]["passwd"].value;
+    var errorsPasswd = document.getElementById('passwdError');
+    if(aadhar==null || aadhar=="" || aadhar.length>12 || aadhar.length<12){
+        errorsAadhar.innerHTML="Invalid Aadhar Number";
+        errorsAadhar.style.display="block";
+        return false;
+    }
+    else if(mobile==null || mobile=="" || mobile.length<10){
+        errorsMobile.innerHTML="Username/Mobile Number must be of minimum 10 characters";
+        errorsMobile.style.display="block";
+        return false;
+       
+    }
+    else if(passwd==null || passwd=="" || passwd.length<6){
+        errorsPasswd.innerHTML="Password must cantain minimum 6 characters";
+        errorsPasswd.style.display="block";
+        return false;
+    }
+    return true;
+    
 }
-
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
-
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
-
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
-
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
-
-        // Perform your AJAX/Fetch login
-
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
-
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "Username must be at least 10 characters in length");
-            }
-        });
-
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
-});
